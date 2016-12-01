895464c080a430a182684da2ccf1005048c4a2d9
wangliu <wangliu@isoftstone.com>
2016-12-01 22:39:51 +0800

    init project.

---
+++ app.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+var apiRouter = require('./router/service.js');
+
+app.use('/api', apiRouter);
+app.listen(3030);
+console.log('api service started on port:3030 http://localhost:3030');
---
+++ router/service.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+router.get('/test', function (req, res, next) {
+    res.json({success: true});
+});
+module.exports = router;
