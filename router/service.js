4b747ee11ebdb2ff29806b8c06f22f9d4875f172
wangliu <wangliu@isoftstone.com>
2017-01-08 10:50:43 +0800

    update.

--- router/service.js
+++ router/service.js
@@ -2,7 +2,7 @@ var express = require('express');
 var exec = require('child_process').exec;
 var router = express.Router();
 router.get('/shutdown', function (req, res, next) {
-    var cmdStr = 'shutdown -s -t 30';
+    var cmdStr = 'shutdown -s -t 60';
     exec(cmdStr, function (err, stdout, stderr) {
         console.log(stdout);
         res.json({success: true});
